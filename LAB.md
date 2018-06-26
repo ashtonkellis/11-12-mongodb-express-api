795d19a80686d4c83394958c71142e7b3401ead3
Ashton K Ellis <ashtonkellis@gmail.com>
2018-06-26 14:45:32 -0700

    refactored to error middleware

--- LAB.md
+++ LAB.md
@@ -1,10 +1,27 @@
 ## Submission Instructions
-* Create a pull request from your lab branch branch to your `master` branch
-* Open a pull request to this repository
-* Submit on canvas 
+* Continue working from your previous lab, but switch to a new branch called `lab-12`.
+* Create a pull request from your branch to your `master` branch
+* Set up Travis on your forked repo
+* Deploy your API to Heroku
+* Submit on canvas:
+  * a question and observation
+  * your original estimate
+  * how long you spent
+  * a link to your pull request
+  * a link to your Heroku deployment
 
-a question: i can't figure out why i can't get the database to actually clear after every test. It is not clearing between my GET all and GET?:id tests, no matter what i seem to try :(
-and observation: async testing is really really hard, apparently.
-your original estimate: 5 hours (mostly due to overactive bladder and a 30 minute round trip to the restroom at the armory, while the restrooms were being fixed)
-how long you spent: ~5 hours
-and a link to your pull request: https://github.com/ashtonkellis/11-12-mongodb-express-api/pull/4
+## Learning Objectives  
+* students will be able to work with application, router, and 3rd party middleware through the use of express.js
+* students will be able to implement custom middleware through the use of express.js
+* students will be able to create custom routers for a specific resource
+
+## Requirements
+For this assignment you will be building a RESTful HTTP server using express.
+
+### Feature Tasks  
+#### Error Middleware
+Create an error middleware for handling all error responses from your server. Refactor all your routes to utilize the error middleware by passing errors to `next`.
+
+  
+#### Documentation
+Same as Lab 11
--- src/router/dinosaur-router.js
+++ src/router/dinosaur-router.js
@@ -8,11 +8,15 @@ import logger from '../lib/logger';
 const jsonParser = bodyParser.json();
 const dinosaurRouter = new Router();
 
-dinosaurRouter.post('/api/dinosaurs', jsonParser, (request, response) => {
+dinosaurRouter.post('/api/dinosaurs', jsonParser, (request, response, next) => {
   logger.log(logger.INFO, 'DINOSAUR-ROUTER POST to /api/dinosaurs - processing a request');
   if (!request.body.name) {
-    logger.log(logger.INFO, 'DINOSAUR-ROUTER POST /api/dinosaurs: Responding with 400 error for no name');
-    return response.sendStatus(400);
+    const err = new Error('Name required');
+    err.status = 400;
+    next(err);
+    
+    // logger.log(logger.INFO, 'DINOSAUR-ROUTER POST /api/dinosaurs: Responding with 400 error for no name');
+    // return response.sendStatus(400);
   }
 
   Dinosaur.init()
@@ -23,33 +27,11 @@ dinosaurRouter.post('/api/dinosaurs', jsonParser, (request, response) => {
       logger.log(logger.INFO, `DINOSAUR-ROUTER POST:  a new dinosaur was saved: ${JSON.stringify(newDinosaur)}`);
       return response.json(newDinosaur);
     })
-    .catch((err) => {
-      // we will hit here if we have some misc. mongodb error or parsing id error
-      if (err.message.toLowerCase().includes('cast to objectid failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 404 status code to mongdb error, objectId ${request.params.id} failed, ${err.message}`);
-        return response.sendStatus(404);
-      }
-
-      // a required property was not included, i.e. in this case, "name"
-      if (err.message.toLowerCase().includes('validation failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 400 status code for bad request ${err.message}`);
-        return response.sendStatus(400);
-      }
-      // we passed in a name that already exists on a resource in the db because in our Dinosaur model, we set name to be "unique"
-      if (err.message.toLowerCase().includes('duplicate key')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 409 status code for dup key ${err.message}`);
-        return response.sendStatus(409);
-      }
-
-      // if we hit here, something else not accounted for occurred
-      logger.log(logger.ERROR, `DINOSAUR-ROUTER GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
-      return response.sendStatus(500); // Internal Server Error
-    });
+    .catch(next);
   return undefined;
 });
 
-// you need this question mark after ":id" or else Express will skip to the catch-all in lib/server.js 
-dinosaurRouter.get('/api/dinosaurs/:id?', (request, response) => {
+dinosaurRouter.get('/api/dinosaurs/:id?', (request, response, next) => {
   logger.log(logger.INFO, 'DINOSAUR-ROUTER GET /api/dinosaurs/:id = processing a request');
 
   if (!request.params.id) {
@@ -58,45 +40,32 @@ dinosaurRouter.get('/api/dinosaurs/:id?', (request, response) => {
         logger.log(logger.INFO, 'DINOSAUR-ROUTER GET /api/dinosaurs responding with 200 code for successful get');
         return response.json(dinosaurs);
       })
-      .catch((err) => {
-        // we will hit here if we have a mongodb error or parsing id error
-        if (err.message.toLowerCase().includes('cast to objectid failed')) {
-          logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 404 status code to mongdb error, objectId ${request.params.id} failed`);
-          return response.sendStatus(404);
-        }
-  
-        // if we hit here, something else not accounted for occurred
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
-        return response.sendStatus(500);
-      });
+      .catch(next);
   }
 
+  // TODO: refactor this to use find by id and delete or find by id and remove
   return Dinosaur.findOne({ _id: request.params.id })
     .then((dinosaur) => {
       if (!dinosaur) {
-        logger.log(logger.INFO, 'DINOSAUR-ROUTER GET /api/dinosaurs/:id: responding with 404 status code for no dinosaur found');
-        return response.sendStatus(404);
+        const err = new Error(`Dinosaur # ${request.params.id} not found`);
+        err.status = 404;
+        next(err);
+        // logger.log(logger.INFO, 'DINOSAUR-ROUTER GET /api/dinosaurs/:id: responding with 404 status code for no dinosaur found');
+        // return response.sendStatus(404);
       }
       logger.log(logger.INFO, 'DINOSAUR-ROUTER GET /api/dinosaurs/:id: responding with 200 status code for successful get');
       return response.json(dinosaur);
     })
-    .catch((err) => {
-      // we will hit here if we have a mongodb error or parsing id error
-      if (err.message.toLowerCase().includes('cast to objectid failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 404 status code to mongdb error, objectId ${request.params.id} failed`);
-        return response.sendStatus(404);
-      }
-
-      // if we hit here, something else not accounted for occurred
-      logger.log(logger.ERROR, `DINOSAUR-ROUTER GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
-      return response.sendStatus(500);
-    });
+    .catch(next);
 });
 
-dinosaurRouter.put('/api/dinosaurs/:id?', jsonParser, (request, response) => {
+dinosaurRouter.put('/api/dinosaurs/:id?', jsonParser, (request, response, next) => {
   if (!request.params.id) {
-    logger.log(logger.INFO, 'DINOSAUR-ROUTER PUT /api/dinosaurs: Responding with a 400 error code for no id passed in');
-    return response.sendStatus(400);
+    const err = new Error('Id required');
+    err.status = 400;
+    next(err);
+    // logger.log(logger.INFO, 'DINOSAUR-ROUTER PUT /api/dinosaurs: Responding with a 400 error code for no id passed in');
+    // return response.sendStatus(400);
   }
 
   const options = {
@@ -112,41 +81,24 @@ dinosaurRouter.put('/api/dinosaurs/:id?', jsonParser, (request, response) => {
       logger.log(logger.INFO, `DINOSAUR-ROUTER PUT - responding with a 200 status code for successful updated dinosaur: ${JSON.stringify(updatedDinosaur)}`);
       return response.json(updatedDinosaur);
     })
-    .catch((err) => {
-      // we will hit here if we have some misc. mongodb error or parsing id error
-      if (err.message.toLowerCase().includes('cast to objectid failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 404 status code to mongdb error, objectId ${request.params.id} failed, ${err.message}`);
-        return response.sendStatus(404);
-      }
-
-      // a required property was not included, i.e. in this case, "name"
-      if (err.message.toLowerCase().includes('validation failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 400 status code for bad request ${err.message}`);
-        return response.sendStatus(400);
-      }
-      // we passed in a name that already exists on a resource in the db because in our Dinosaur model, we set name to be "unique"
-      if (err.message.toLowerCase().includes('duplicate key')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER PUT: responding with 409 status code for dup key ${err.message}`);
-        return response.sendStatus(409);
-      }
-
-      // if we hit here, something else not accounted for occurred
-      logger.log(logger.ERROR, `DINOSAUR-ROUTER GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
-      return response.sendStatus(500);
-    });
+    .catch(next);
   return undefined;
 });
 
-dinosaurRouter.delete('/api/dinosaurs/:id?', (request, response) => {
+dinosaurRouter.delete('/api/dinosaurs/:id?', (request, response, next) => {
   logger.log(logger.INFO, 'DINOSAUR-ROUTER DELETE /api/dinosaurs/:id = processing a request');
 
-  if (!request.params.id) {
-    return response.sendStatus(404);
-  }
+  if (!request.params.id) return response.sendStatus(404);
 
+  // TODO: refactor to use find by id and delete, or find by id and remove
   return Dinosaur.deleteOne({ _id: request.params.id })
     .then((data) => {
       if (!data.n) {
+        // TODO: figure out why this isn't working!
+        // const err = new Error('Id required');
+        // err.status = 400;
+        // next(err);
+        
         logger.log(logger.INFO, 'DINOSAUR-ROUTER DELETE /api/dinosaurs/:id responding with 404 status code for no dinosaur found');
         return response.sendStatus(400);
       }
@@ -154,17 +106,7 @@ dinosaurRouter.delete('/api/dinosaurs/:id?', (request, response) => {
       logger.log(logger.INFO, 'DINOSAUR-ROUTER DELETE api/dinosaurs responding with 204 code for successful delete');
       return response.sendStatus(204);
     })
-    .catch((err) => {
-      // we will hit here if we have a mongodb error or parsing id error
-      if (err.message.toLowerCase().includes('cast to objectid failed')) {
-        logger.log(logger.ERROR, `DINOSAUR-ROUTER DELETE: responding with 404 status code to mongdb error, objectId ${request.params.id} failed`);
-        return response.sendStatus(404);
-      }
-
-      // if we hit here, something else not accounted for occurred
-      logger.log(logger.ERROR, `DINOSAUR-ROUTER DELETE: 500 status code for unaccounted error ${JSON.stringify(err)}`);
-      return response.sendStatus(500);
-    });
+    .catch(next);
 });
 
 export default dinosaurRouter;
